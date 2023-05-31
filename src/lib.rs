4d9c3b0da630d11a284d842735cbcf309e98bdf9
William R. Fraser <wfraser@codewise.org>
2023-05-31 16:13:50 -0700

    make the README doc be at crate level

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-#[doc = include_str!("../README.md")]
+#![doc = include_str!("../README.md")]
+#![deny(missing_docs)]
 
 /// Define a const map and a const lookup function as associated items of a struct.
 ///
